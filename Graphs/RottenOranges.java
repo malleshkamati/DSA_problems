fdd2bdcb813adf73c0f07d25e91211d08b80b671
mallesh <malleshkamati0803@gmail.com>
2025-12-18 19:40:29 +0530

    rotten orange

--- Graphs/RottenOranges.java
+++ Graphs/RottenOranges.java
@@ -1,8 +1,14 @@
 package DSA_problems.Graphs;
 
 import java.util.*;
-import javafx.util.Pair;
+// You are given an m x n grid where each cell can have one of three values:
 
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 public class RottenOranges {
     static class Pair<K, V> {
     private K key;
